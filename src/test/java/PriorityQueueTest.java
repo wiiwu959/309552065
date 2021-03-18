348d5e571b4370a68913644cfc330e1566917f68
Wiiwu <iriswu959@gmail.com>
2021-03-19 00:50:58 +0800

    fix the error.

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.Assert;
 
 import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
